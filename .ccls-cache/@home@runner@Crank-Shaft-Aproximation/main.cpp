0a1b2b59438c3b3c83b9c51e8ea6154a2912b222
Dillon Hendrix <111817074+djhendrix2@users.noreply.github.com>
2022-08-29 15:51:06 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Crank-Shaft-Aproximation/main.cpp
@@ -0,0 +1,22 @@
+/**********************************************************************************
+Programmer: Dillon Hendrix
+Date: 08/29/2022
+Course: CSCI 130
+Description: This code is writen to solve the problem of finding a crank are raduis. The   user will input the the length of the crank arm in mm. The user will also be prompted to   input the force from the rider (lbs) and allowabe stress (s) in psi. The program will      then calculate the radius (r) of the rod by: 
+  r^3 = (l*p)/(pi*s)
+***********************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // initialize variables
+  int p, l, s, r;
+
+  // input variables 
+  p = 300;
+  l = 170;
+  s = 10000;
+  
+  cout << ;
+}
